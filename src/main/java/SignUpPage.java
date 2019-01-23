f5518b78a93cb51dafb43aad7136d77b53723790
AndyRivers83 <andykolesinski@gmail.com>
2019-01-23 17:24:35 +0300

    no message

--- src/main/java/SignUpPage.java
+++ src/main/java/SignUpPage.java
@@ -1,36 +1,98 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class SignUpPage {
 
-    WebDriver driver;
+    private WebDriver driver;
 
     public SignUpPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    By emailField = By.cssSelector("input#register-email");
-    By confirmEmailField = By.cssSelector("input#register-confirm-email");
-    By passwordField = By.cssSelector("input#register-password");
-    By nameField = By.cssSelector("input#register-displayname");
-    By monthDropDown = By.cssSelector("input#register-dob-month");
-    String monthDropDownOption = "//select[@id='register-dob-month']/option[text()='%s']";
-    By dayField = By.cssSelector("input#register-dob-day");
-    By yearField = By.cssSelector("input#register-dob-year");
-    String sexRadioButton = "//li[@id='li-gender']/label[normalize-space()='Male']/input";
-    By shareCheckbox = By.cssSelector("input#register-thirdparty");
-    By registerButton = By.cssSelector("a#register-button-email-submit");
-    By errorLabel = By.xpath("//label[@class='has-error' and string-length(text())>0]");
-    String errorByText = "//label[@class='has-error' and text()='%s']";
+    private By emailField = By.cssSelector("input#register-email");
+    private By confirmEmailField = By.cssSelector("input#register-confirm-email");
+    private By passwordField = By.cssSelector("input#register-password");
+    private By nameField = By.cssSelector("input#register-displayname");
+    private By monthDropDown = By.cssSelector("input#register-dob-month");
+    private String monthDropDownOption = "//select[@id='register-dob-month']/option[text()='%s']";
+    private By dayField = By.cssSelector("input#register-dob-day");
+    private By yearField = By.cssSelector("input#register-dob-year");
+    private String sexRadioButton = "//li[@id='li-gender']/label[normalize-space()='%s']/input";
+    private By shareCheckbox = By.cssSelector("input#register-thirdparty");
+    private By registerButton = By.cssSelector("a#register-button-email-submit");
+    private By errorLabel = By.xpath("//label[@class='has-error' and string-length(text())>0]");
+    private String errorByText = "//label[@class='has-error' and text()='%s']";
 
+    public SignUpPage typeEmail(String email){
+        driver.findElement(emailField).sendKeys(email);
+        return this;
+    }
 
+    public SignUpPage typeConfirmEmail(String email){
+        driver.findElement(confirmEmailField).sendKeys(email);
+        return this;
+    }
 
+    public SignUpPage typePassword(String password){
+        driver.findElement(passwordField).sendKeys(password);
+        return this;
+    }
 
+    public SignUpPage typeName(String name){
+        driver.findElement(nameField).sendKeys(name);
+        return this;
+    }
 
+    public SignUpPage setMonth(String month){
+        driver.findElement(monthDropDown).click();
+        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(String.format(monthDropDownOption, month)))).click();
+        return this;
+    }
 
+    public SignUpPage typeDay(String day){
+        driver.findElement(dayField).sendKeys(day);
+        return this;
+    }
 
+    public SignUpPage typeYear(String year){
+        driver.findElement(yearField).sendKeys(year);
+        return this;
+    }
 
+    public SignUpPage typeSex(String value){
+        driver.findElement(By.xpath(String.format(sexRadioButton, value))).sendKeys(value);
+        return this;
+    }
 
+    public SignUpPage setShare(boolean value){
+        WebElement checkbox = driver.findElement(shareCheckbox);
+        if(!checkbox.isSelected() == value) {
+            checkbox.click();
+        }
+        return this;
+    }
+
+    public void clickSignUpButton(){
+        driver.findElement(registerButton).click();
+    }
+
+     public List<WebElement> getErrors(){
+        return driver.findElements(errorLabel);
+     }
+
+    public String getErrorByNumber(int number){
+        return getErrors().get(number - 1).getText();
+    }
+
+    public boolean getErrorVisible(String message){
+        return driver.findElements(By.xpath(String.format(errorByText, message))).size()>0
+                &&  driver.findElements(By.xpath(String.format(errorByText, message))).get(0).isDisplayed();
+    }
 
 
 }
